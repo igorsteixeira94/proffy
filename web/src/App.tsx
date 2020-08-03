610e1555f17692a88c36b925bfff25a8d0d76bdb
Igorsteixeira94 <igorsteixeira94@gmail.com>
2020-08-03 19:33:38 -0300

    Feature: Landing Proffy and Header pages teachers

---
+++ web/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './assets/styles/global.css';
+import Landing from '../src/pages/Landing'
+import Routes from './routes';
+
+function App() {
+  return (<Routes></Routes>)
+}
+
+export default App;
---
+++ web/src/pages/TeacherForm/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PageHeader from '../../components/PageHeader';
+
+function TeacherForm() {
+  return (
+    <div id="page-teacher-list" className="container">
+      <PageHeader title="Não sei a frase">
+      </PageHeader>
+    </div>
+  );
+}
+
+export default TeacherForm;
