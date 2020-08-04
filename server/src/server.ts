9ac15708b56379831b63e3bdf735fc187054d0b1
Igorsteixeira94 <igorsteixeira94@gmail.com>
2020-08-04 09:44:53 -0300

    Config: knex and sqlite in ts

---
+++ server/src/server.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (request, response) => {
+
+  return response.json("Hello Word");
+});
+
+app.listen(3333, () => {
+  console.log("Server Running");
+});
