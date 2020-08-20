c95233a99769a4f4bbe2bafa97a84941cf2478a9
Igor Rodrigues <igorsteixeira94@gmail.com>
2020-08-20 11:41:34 -0300

    Feature: ProfileForm Web

--- web/src/pages/Profile/index.tsx
+++ web/src/pages/Profile/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, FormEvent } from 'react';
 import HeaderFix from '../../components/HeaderFix';
 import PageHeader from '../../components/PageHeader';
 
@@ -11,7 +11,9 @@ import cameraIcon from '../../assets/images/icons/camera.svg';
 
 import './styles.css'
 import { phoneMask, moneyMask } from '../../utils/mask';
+import { useHistory } from 'react-router-dom';
 function Profile() {
+    const history = useHistory();
     const [img, setImg] = useState("https://avatars1.githubusercontent.com/u/47749249?s=460&u=9c6deccd060caa4aa48381692fda430ab15af8de&v=4");
     const [whatsapp, setWhatsapp] = useState('(  ) _  ____ - ____');
     const [cost, setCost] = useState('R$ 0,00');
@@ -23,12 +25,37 @@ function Profile() {
     function addNewScheduleItem() {
         setScheduleItems([...scheduleItems, { week_day: 0, from: '', to: '' }])
     }
+
+    function setScheduleItemValue(position: number, field: string, value: string) {
+        const newArray = scheduleItems.map((scheduleItem, index) => {
+          if (index === position) {
+            return { ...scheduleItem, [field]: value };
+          }
+          return scheduleItem;
+        });
+    
+        setScheduleItems(newArray);
+    }
+
+    function deleteNewScheduleItem(id:number){
+        
+        if(scheduleItems.length === 1){
+            setScheduleItems([{ week_day: 0, from: '', to: '' }]);
+        }else{
+
+            const newArray = scheduleItems.filter((item,index)=>{ return index !== id });
+            setScheduleItems(newArray);
+        }
+
+    }
+
     function handleChangeWhatsapp(value: string) {
         setWhatsapp(phoneMask(value));
     }
     function handleChangeCost(value: string) {
         setCost(moneyMask(value));
     }
+
 
     function handleImageProfile(e: React.ChangeEvent<HTMLInputElement> | any) {
         e.preventDefault();
@@ -40,6 +67,10 @@ function Profile() {
             reader.readAsDataURL(file);
         }
 
+    }
+    function handleCreateClass(e: FormEvent) {
+        e.preventDefault();
+        history.push('/landing');
     }
     return (
         <div id="page-profile-form" className="container">
@@ -62,7 +93,7 @@ function Profile() {
             </PageHeader>
 
             <main>
-                <form >
+                <form onSubmit={handleCreateClass}>
                     <fieldset>
                         <legend>Seus dados</legend>
 
@@ -124,44 +155,53 @@ function Profile() {
                         />
 
                     </fieldset>
+
                     <fieldset>
                         <legend>
                             Horários disponíveis
-              <button type="button" onClick={addNewScheduleItem}>
+                            <button type="button" onClick={addNewScheduleItem}>
                                 + Novo
-              </button>
+                            </button>
                         </legend>
                         {scheduleItems.map((scheduleItem, index) => {
-                            return (
-                                <div className="schedule-item">
-                                    <Select
-                                        name="week_day"
-                                        label="Dia da semana"
-
-                                        options={[
-                                            { value: '0', label: 'Domingo' },
-                                            { value: '1', label: 'Segunda-feira' },
-                                            { value: '2', label: 'Terça-feira' },
-                                            { value: '3', label: 'Quarta-feira' },
-                                            { value: '4', label: 'Quinta-feira' },
-                                            { value: '5', label: 'Sexta-feira' },
-                                            { value: '6', label: 'Sábado' },
+                        return (
+                            <div key={index} className="schedule-item">
+                                <Select
+                                    name="week_day"
+                                    label="Dia da semana"
+                                    value={scheduleItem.week_day}
+                                    onChange={e => { setScheduleItemValue(index, 'week_day', e.target.value) }}
+                                    options={[
+                                        { value: '0', label: 'Domingo' },
+                                        { value: '1', label: 'Segunda-feira' },
+                                        { value: '2', label: 'Terça-feira' },
+                                        { value: '3', label: 'Quarta-feira' },
+                                        { value: '4', label: 'Quinta-feira' },
+                                        { value: '5', label: 'Sexta-feira' },
+                                        { value: '6', label: 'Sábado' },
                                         ]}
-                                    />
-                                    <Input
-                                        type="time"
-                                        name="from"
-                                        label="Das"
+                                />
+                                <Input
+                                    type="time"
+                                    name="from"
+                                    label="Das"
+                                    value={scheduleItem.from}
+                                    onChange={e => { setScheduleItemValue(index, 'from', e.target.value) }}
 
 
-                                    />
-                                    <Input
-                                        type="time"
-                                        name="to"
-                                        label="Até"
+                                />
+                                <Input
+                                    type="time"
+                                    name="to"
+                                    label="Até"
+                                    value={scheduleItem.to}
+                                    onChange={e => { setScheduleItemValue(index, 'to', e.target.value) }}
 
 
-                                    />
+                                />
+                                <fieldset className="delete">
+                                    <legend onClick={()=>{deleteNewScheduleItem(index)}}>Excluir horário</legend>
+                                 </fieldset>
 
                                 </div>
                             )
@@ -174,7 +214,7 @@ function Profile() {
             Importante! <br />
             Preencha todos os dados
           </p>
-                        <button type="submit">Salvar cadastro</button>
+                        <button type="submit">Atualizar cadastro</button>
                     </footer>
                 </form>
             </main>
